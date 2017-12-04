1b3104db4e2d8dcb71305caf462a573d32a1e71f
Xrok <sercar88@gmail.com>
2017-12-04 03:08:11 -0500

    colecciones almost done !

--- sugarsand.js
+++ sugarsand.js
@@ -1,11 +1,20 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+
     $('.main_opc').hover(function(){
 		$(this).find('.sub_menu').show();
     }, function(){
     	$(this).find('.sub_menu').hide();
     });
  
+    $('.mini').click(function(e){
+    	e.preventDefault();
+    	var year = $(this).data('year');
+    	var num = $(this).data('number');
+    	var new_bg = '<img src="../imagenes/'+year+'_'+num+'.jpg">';
+    	$(this).closest('.contenido').find('.show').html(new_bg);
+    	$('html,body').animate({scrollTop: $('.show').offset().top},'fast');
+    })
 });
 
 
